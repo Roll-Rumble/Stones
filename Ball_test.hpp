d166242f32ea755632fcb066ff53f84e7e49b064
ManofRenown <orlan.forshaw22@ic.ac.uk>
2024-02-29 22:58:32 +0000

    Updated style choices

--- Ball_test.hpp
+++ Ball_test.hpp
@@ -17,45 +17,23 @@ public:
 	}
 	~Ball();
 
-//			*				1
-//		*		*		2		3	// this is basic hitbox and id
-//			*				4
+ //			*				1
+ //		*		*		2		3	// this is basic hitbox and id
+ //			*				4
 	void Collision(int hitbox_ID) // idea here is that the id tells the function which direction the collision has occurred 
 	{ 
-		switch (hitbox_ID){
-			case 1 : velocity[1] = -velocity[1];
-			case 2 : velocity[0] = -velocity[0];
-			case 3 : velocity[0] = -velocity[0];
-			case 4 : velocity[1] = -velocity[1];
-		} // this is obviously a very basic version
+
 	}
 
-	void Accelerate(std::vector<int> input){
-		int index; // placeholder
-		if(input.at(index) < 0){
-			// negative acceleration
-		}
-		else if(input.at(index) > 0){
-			// positive acceleration
-		}
-		else return; //acceleration is zero so no change needed
+	void Stop()
+	{
+
 	}
 
-	void Stop(){
-		velocity[0,1] = 0;
-	}
+	void Hold()
+	{
 
-	void Hold(){
-		velocity[0,1] = 0;
-		mobile = 0;
-		
 	}
-};
-
-Ball::Ball(/* args */)
-{
 }
 
-Ball::~Ball()
-{
-}
+
