89f6ab514e6a64d90fdf55eff0faa3cda2a01fe4
ccrownhill <ckronbichler@proton.me>
2024-03-03 14:03:37 +0000

    make circle move with nios2

--- Constantin/fpga_move/src/controller.cpp
+++ Constantin/fpga_move/src/controller.cpp
@@ -4,79 +4,92 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
-#include <thread>
 
 
 #include "controller.hpp"
 
 
-//#define STR_BUFFER_SIZE 50
-//#define READ_BUFFER_SIZE 1
 
-int16_t Controller::getXVal()
+Controller::Controller() : x_val {0}, y_val {0}, z_val {0}
+{
+    handle = jtagatlantic_open(NULL, -1, -1, "jtag_read");
+    int err;
+    if (err = jtagatlantic_cable_warning(handle)) {
+        std::cerr << "Error with connection: error code " << err << "\n";
+        std::exit(1);
+    }
+}
+
+Controller::~Controller()
+{
+    jtagatlantic_close(handle);
+}
+
+short Controller::getXVal()
 {
 	return x_val;
 }
 
-int16_t Controller::getYVal()
+short Controller::getYVal()
 {
 	return y_val;
 }
 
-int16_t Controller::getZVal()
+short Controller::getZVal()
 {
 	return z_val;
 }
 
-void Controller::startNewThread()
+
+std::pair<float, float> Controller::get_xy()
 {
-    std::thread read_thread(*this);
-}
-
-void Controller::operator()()
-{
-    const char* progname = "jtag_read";
-    JTAGATLANTIC* handle = jtagatlantic_open(NULL, -1, -1, progname);
-
-    //char read_buffer[READ_BUFFER_SIZE + 1];
-    //read_buffer[READ_BUFFER_SIZE] = '/0';
-    //unsigned int bytes_read;
-
-    char read_buffer[1];
     unsigned int bytes_read;
     unsigned int bytes_available;
 
-    char str_buffer[50];
-    unsigned int str_buf_idx = 0;
+    static char str_buffer[STR_BUFFER_SIZE];
+    static unsigned int str_buf_idx = 0;
 
-    if (jtagatlantic_cable_warning(handle)) {
-        std::cout << "Error with connection: error code " << jtagatlantic_cable_warning(handle) << "\n";
-        assert(0);
+
+    
+    //std::cout << "called" << std::endl;
+    // std::cout << "Bytes available to read is " << jtagatlantic_bytes_available(handle) << "\n";
+    bytes_available = jtagatlantic_bytes_available(handle);
+    //std::cout << "Bytes available: " << bytes_available << "\n";
+    if (bytes_available) {
+        str_buf_idx += jtagatlantic_read(handle, str_buffer + str_buf_idx, READ_BUFFER_SIZE);
+        //read_buffer[bytes_read] = '/0';     // Create null-terminating string from input
+        //std::cout << "Read " << bytes_read << "bytes\n";
+        //std::cout << "Read data: " << *read_buffer << "\n";
+        
+        if (str_buf_idx > 0 && str_buffer[str_buf_idx - 1] == 'e') {
+            str_buffer[str_buf_idx - 1] = '\0';
+            std::cout << "GOT RESULT: " << str_buffer << "\n";
+            str_buf_idx = 0;
+            sscanf(str_buffer, "X: %hd, Y: %hd, Z: %hd", &x_val, &y_val, &z_val);
+            memset(str_buffer, '\0', STR_BUFFER_SIZE);
+        }
     }
 
-    while (true) {
-        // std::cout << "Bytes available to read is " << jtagatlantic_bytes_available(handle) << "\n";
-        bytes_available = jtagatlantic_bytes_available(handle);
-        //std::cout << "Bytes available: " << bytes_available << "\n";
-        if (bytes_available) {
-            bytes_read = jtagatlantic_read(handle, read_buffer, 1);
-            //read_buffer[bytes_read] = '/0';     // Create null-terminating string from input
-            //std::cout << "Read " << bytes_read << "bytes\n";
-            //std::cout << "Read data: " << *read_buffer << "\n";
-            str_buffer[str_buf_idx] = *read_buffer;
-            str_buf_idx++;
-            if (*read_buffer == 'e') {
-                str_buffer[str_buf_idx - 1] = '\0';
-                std::cout << "GOT RESULT: " << std::string(str_buffer) << "\n";
-                str_buf_idx = 0;
-                sscanf(str_buffer, "X: %i, Y: %i, Z: %i", &(this->x_val), &(this->y_val), &(this->z_val));
-            }
-        }
-        else {
-            //std::cout << "Wasted cycle!\n";
-        }
+    return std::make_pair(static_cast<float>(x_val), static_cast<float>(y_val));
+}
 
+std::pair<float, float> Controller::normalise_xy(float x, float y)
+{
+    if (std::abs(x) < INPUT_THRESHOLD) {
+        x = 0;
     }
 
-    std::cout << "Error: JTAG connection broken\n";
+    if (std::abs(y) < INPUT_THRESHOLD) {
+        y = 0;
+    }
+
+    if (x > OUTLIER_THRESHOLD) {
+        x = OUTLIER_THRESHOLD;
+    }
+
+    if (y > OUTLIER_THRESHOLD) {
+        y = OUTLIER_THRESHOLD;
+    }
+
+    return std::make_pair(-x / OUTLIER_THRESHOLD, -y / OUTLIER_THRESHOLD);
 }
