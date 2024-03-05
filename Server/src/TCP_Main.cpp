bf3a09dd023cc0bdf1b23305b5fc955b4b800bb1
ManofRenown <orlan.forshaw22@ic.ac.uk>
2024-03-05 16:31:11 +0000

    adding TCP_Main

---
+++ Server/src/TCP_Main.cpp
@@ -0,0 +1,26 @@
+#include "TCP_Function.cpp"
+#include "Ball_test.hpp"
+#include "Wall_test.hpp"
+#include "Collision_detect.hpp"
+
+
+int main()
+{
+	int wall_num =1;
+	Ball ball(0, 0, 5);
+	std::vector<Wall> walls;
+	Wall wall1(-1, -1, 1, 1);
+	walls.push_back(wall1);
+
+	int input_x = 1; // get from accelerometer
+	int input_y = 1;
+	
+	ball.Update(input_x, input_y);
+
+	
+	
+
+	//int_fast8_t decision = Collision_detect(ball, wall1);
+	//std::cout << decision << std::endl;
+	return 0;
+}
