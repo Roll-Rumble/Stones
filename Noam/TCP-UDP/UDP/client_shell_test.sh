41c29dd6483e305ffb04b62c8f51b1958bc2e6d7
Ryan Voecks <voecksryan@gmail.com>
2024-03-11 12:44:53 +0000

    Deleted not needed files

--- Noam/TCP-UDP/UDP/client_shell_test.sh
+++
@@ -1,5 +0,0 @@
-$echo = New-Object System.Net.Sockets.UdpClient('3.8.190.125', 12000)
-$sendbytes = [System.Text.Encoding]::ASCII.GetBytes("test message")
-$echo.Send($sendbytes, $sendbytes.Length)
-$echo.Close()
-// to run this run  nc -u -l 12000 on server
