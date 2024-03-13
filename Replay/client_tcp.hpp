20a737ce124ffef1d2a82be118ae9dcd5a3e5f2e
noamweit <weitzmanoam@gmail.com>
2024-03-13 13:37:28 +0000

    fixed pack and unpack, functions work for local machine, need to implement server

---
+++ Replay/client_tcp.hpp
@@ -0,0 +1,27 @@
+#ifndef CLIENT_TCP_HPP
+#define CLIENT_TCP_HPP
+
+#include <string>
+#include <winsock2.h>
+
+
+// TODO: MOVE THIS TO NETUTIL
+#define SERVER_TCP_PORT 13000
+
+
+#define SEND_BUF_SIZE
+#define RECEIVE_BUF_SIZE
+
+class TCP_Client {
+public:
+    TCP_Client();
+    ~TCP_Client();
+
+    void send_data(char buffer[SEND_BUF_SIZE]);
+    void receive(char buffer[RECEIVE_BUF_SIZE]);
+
+private:
+    SOCKET socket_;
+};
+
+#endif
--- Replay/replay_packing.cpp
+++ Replay/replay_packing.cpp
@@ -3,14 +3,12 @@
 #include "game_util.hpp"
 #include "client_tcp.hpp"
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 32
 #define INT16_SIZE 2
 
-void pack32(char *buffer, uint32_t xy) {
-    buffer[0] = (xy >> 24) & 0xFF; //  we AND with 0xFF to ensure that the value is 8 only bits long (= 1 byte)
-    buffer[1] = (xy >> 16) & 0xFF;
-    buffer[2] = (xy >> 8) & 0xFF;
-    buffer[3] = xy & 0xFF;
+void pack32(char *buf, uint32_t xy) {
+    *buf++ = xy>>24; *buf++ = xy>>16;
+    *buf++ = xy>>8;  *buf++ = xy;
 }
 
 
@@ -20,24 +18,73 @@ void encode_input(char *buffer, int16_t x, int16_t y) {
 }
 
 
-void Convert_to_Buffers(const std::vector< std::vector<XYPairInt16> > &replay_data) {
-    char data_buffer[BUFFER_SIZE];
-    unsigned int buffer_index = 0;
-    TCP_Client TCPclient;
+// void Convert_to_Buffers(const std::vector< std::vector<XYPairInt16> > &replay_data) {
+//     char data_buffer[BUFFER_SIZE];
+//     unsigned int buffer_index = 0;
+//     TCP_Client TCPclient;
 
-    for (const auto& frame : replay_data) {
-        for (const auto& xy_pair : frame) { // each frame has 2 pairs, one for each ball
+//     for (const auto& frame : replay_data) {
+//         for (const auto& xy_pair : frame) { // each frame has 2 pairs, one for each ball
+//             encode_input(data_buffer, xy_pair.x, xy_pair.y);
+//             buffer_index += 2 * INT16_SIZE; // each pair has 2 int16, x and y
+//             if (buffer_index >= BUFFER_SIZE) {
+//                 TCPclient.send_data(data_buffer);
+//                 buffer_index = 0;
+//             }
+//         }
+//     }
+//     if (buffer_index > 0) {
+//         TCPclient.send_data(data_buffer);
+//     }
+// }
+
+
+void Convert_to_Buffers(const std::vector< std::vector<XYPairInt16> >& replay_data, char* data_buffer) {
+    unsigned int buffer_index = 0;
+
+    for (const std::vector<XYPairInt16>& frame : replay_data) {
+        int i = 0;
+        for (const XYPairInt16& xy_pair : frame) { // each frame has 2 pairs, one for each ball
             encode_input(data_buffer, xy_pair.x, xy_pair.y);
+            data_buffer += 2 * INT16_SIZE;
             buffer_index += 2 * INT16_SIZE; // each pair has 2 int16, x and y
             if (buffer_index >= BUFFER_SIZE) {
-                TCPclient.send_data(data_buffer);
-                buffer_index = 0;
+                // If buffer is full, stop processing
+                return;
             }
         }
+        i = 0;
     }
-    if (buffer_index > 0) {
-        TCPclient.send_data(data_buffer);
+}
+
+
+XYPairInt16 toXYpair(int16_t x, int16_t y) {
+    XYPairInt16 xy;
+    xy.x = x;
+    xy.y = y;
+    return xy;
+}
+
+
+std::vector< std::vector<XYPairInt16> > Convert_to_vector (const char *data_buffer) {
+    std::vector< std::vector<XYPairInt16> > replay_data;
+    std::vector<XYPairInt16> frame;
+
+    unsigned int buffer_index = 0;
+
+    while (buffer_index < BUFFER_SIZE) {
+        int16_t x = (data_buffer[buffer_index++] << 8) + data_buffer[buffer_index++]; // for ball 0
+        int16_t y = (data_buffer[buffer_index++] << 8) + data_buffer[buffer_index++];
+        frame.push_back(toXYpair(x, y));
+
+        x = (data_buffer[buffer_index++] << 8) + data_buffer[buffer_index++]; // for ball 1
+        y = (data_buffer[buffer_index++] << 8) + data_buffer[buffer_index++];
+        frame.push_back(toXYpair(x, y));
+
+        replay_data.push_back(frame);
+        frame.clear();
     }
+    return replay_data;
 }
 
 
@@ -52,4 +99,6 @@ see packing function in top/netutils.cpp
 .
 .
 [last frame, frame n]]
+
+from buffer to vector just do opposite 
 */
