f782682e3df1e9de2277b60bc0f71e21991afbd1
polsan2 <petrojr.mail@gmail.com>
2024-03-13 21:20:44 +0000

    very basic menu and victory screen

--- Constantin/imgui_test/src/main.cpp
+++ Constantin/imgui_test/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <chrono>
+#include <string>
 
 #include "ball.hpp"
 #include "map.hpp"
@@ -45,6 +46,7 @@ int main(void)
     //Rectangle rect1(-0.5f, -0.5f, 1, 1);
     Map map;
     Ball ball(map);
+    int num_of_replays = 3;
 
     bool show_demo_window = true;
     bool show_another_window = false;
@@ -72,6 +74,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* MENU STATE*/
         if (state == GameState::START) {
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
@@ -93,22 +96,19 @@ int main(void)
                 text_horizontally_center("Roll Rumble");
                 ImGui::SetCursorPosY(400);
                 ImGui::Text("Roll Rumble");
+                ImGui::Spacing();
                 if (button_horizontally_center("Start Game", ImVec2(500, 100))) {
                     state = GameState::PLAY;
                 }
-                if (button_horizontally_center("Start Game", ImVec2(500, 100))) {
+                if (button_horizontally_center("Replay", ImVec2(500, 100))) {
                     state = GameState::DB;
                 }
-                ImGui::Checkbox("Use work area instead of main area", &use_work_area);
-                ImGui::SameLine();
-
-                ImGui::CheckboxFlags("ImGuiWindowFlags_NoBackground", &flags, ImGuiWindowFlags_NoBackground);
-                ImGui::CheckboxFlags("ImGuiWindowFlags_NoDecoration", &flags, ImGuiWindowFlags_NoDecoration);
+                if (button_horizontally_center("test: END", ImVec2(500, 100))) {
+                    state = GameState::END;
+                }
+                /*ImGui::SameLine();
                 ImGui::Indent();
-                ImGui::CheckboxFlags("ImGuiWindowFlags_NoTitleBar", &flags, ImGuiWindowFlags_NoTitleBar);
-                ImGui::CheckboxFlags("ImGuiWindowFlags_NoCollapse", &flags, ImGuiWindowFlags_NoCollapse);
-                ImGui::CheckboxFlags("ImGuiWindowFlags_NoScrollbar", &flags, ImGuiWindowFlags_NoScrollbar);
-                ImGui::Unindent();
+                ImGui::Unindent();*/
 
                 //if (p_open && ImGui::Button("Close this window"))
                   //  *p_open = false;
@@ -123,6 +123,75 @@ int main(void)
             map.draw(shader);
             ball.draw(shader);
         }
+        /* REPLAY STATE */
+        else if (state == GameState::DB) {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+            static bool use_work_area = true;
+            static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings;
+
+            // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+            // Based on your use case you may want one or the other.
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+            ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+            if (ImGui::Begin("Example: Fullscreen window", NULL, flags))
+            {
+                ImGui::SetWindowFontScale(3);
+                text_horizontally_center("Replays");
+                ImGui::SetCursorPosY(400);
+                ImGui::Text("Replays");
+
+                for (int i = 0; i < num_of_replays; i++) {
+                    std::string replay_identifier = "REPLAY: " + std::to_string(i);
+                    if (button_horizontally_center(replay_identifier, ImVec2(500, 100))) {
+                        state = GameState::PLAY;
+                    }
+                }
+                if (button_horizontally_center("BACK", ImVec2(500, 100))) {
+                    state = GameState::START;
+                }
+            }
+            ImGui::End();
+
+            // Rendering
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
+        /* END GUI */
+        else if (state == GameState::END) {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+            static bool use_work_area = true;
+            static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings;
+
+            // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+            // Based on your use case you may want one or the other.
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+            ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+            if (ImGui::Begin("Example: Fullscreen window", NULL, flags))
+            {
+                ImGui::SetWindowFontScale(9);
+                text_horizontally_center("You have won!");
+                ImGui::SetCursorPosY(400);
+                ImGui::Text("You have won!");
+            }
+            ImGui::End();
+
+            // Rendering
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
